80aeacbb0678661e578bcc100fe97b7fdc44403b
emandell13 <eric@bluelantern.co>
2025-02-03 15:11:28 -0500

    Refactor to separate out files

---
+++ backend/config/mongoose.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { SocksProxyAgent } = require('socks-proxy-agent');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const fixieData = process.env.FIXIE_SOCKS_HOST.split(new RegExp('[:/@]+'));
+const proxyUrl = `socks5://${fixieData[1]}:${fixieData[2]}@${fixieData[3]}:${fixieData[4]}`;
+const proxyAgent = new SocksProxyAgent(proxyUrl);
+
+const mongooseOptions = {
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+  driverInfo: {
+    name: 'nodejs',
+    version: process.version,
+    platform: process.platform,
+    proxy: proxyAgent
+  }
+};
+
+mongoose.connect(process.env.MONGODB_URI, mongooseOptions)
+  .then(() => console.log('Connected to database'))
+  .catch(error => {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Exit the process with failure
+  });
+
+module.exports = mongoose;
